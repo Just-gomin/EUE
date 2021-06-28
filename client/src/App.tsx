f0131599980348d061ba5cd33934560dbe4420a8
Spark <loot04@korea.ac.kr>
2021-06-28 13:48:39 +0900

    typescript client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,25 @@
-// import { BrowserRouter as Router, Switch } from "react-router-dom";
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-// import Home from './pages/Home'
-import { Button, Row } from "react-bootstrap";
 
 function App() {
-  
   return (
-    <Button>
-      ggg
-    </Button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
