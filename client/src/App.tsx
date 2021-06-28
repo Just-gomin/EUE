ec4d4f56af546ed3efac6d4d2712fe57aff5f29f
Spark <loot04@korea.ac.kr>
2021-06-28 13:28:23 +0900

    tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+// import { BrowserRouter as Router, Switch } from "react-router-dom";
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import Home from './pages/Home'
+import { Button, Row } from "react-bootstrap";
+
+function App() {
+  
+  return (
+    <Button>
+      ggg
+    </Button>
+  );
+}
+
+export default App;
