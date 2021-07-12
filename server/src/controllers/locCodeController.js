650716fca0b8d45463d109bb34a170ac216d16c1
Kangmin An <hkkm612@gmail.com>
2021-07-12 10:14:14 +0900

    Update : 도 정보를 가져오는 코드 및 경로 수정.

--- server/src/controllers/locCodeController.js
+++ server/src/controllers/locCodeController.js
@@ -1,40 +1,13 @@
-import { pool as db, dbMSG } from "../db";
+import db from "../db/index";
 import { serverMSG, statusCode } from "../serverinfo";
 
-// 각각의 지역 코드 정보를 가져오는
-const getQueryResult = async (query) => {
-  let isError = false;
-  let result;
-
-  try {
-    const [row, fields] = await db.execute(query);
-    result = row;
-
-    console.log(dbMSG.query_success);
-  } catch (error) {
-    //Error Log
-    console.log("", error);
-
-    isError = true;
-
-    // 발생한 오류가 DB와 연결 오류인지 확인 후 Error Message 지정 및 전달
-    if (error.code === "ECONNREFUSED") result = dbMSG.connection_err;
-    else result = dbMSG.query_err;
-  }
-
-  return [isError, result];
-};
-
 // Do Code에 대한 GET 요청 처리
-export const getDo = async (req, res) => {
-  const query = "SELECT CODE, DONAME FROM LOCDO";
+export const getDoe = async (req, res) => {
+  const result = await db.Doe.findAll({ logging: false });
 
-  const [isError, result] = await getQueryResult(query);
-
-  if (!isError) {
+  if (result) {
     res.status(statusCode.ok).json({ DO: result });
   } else {
-    console.log(result);
     res.status(statusCode.err).send(serverMSG.server_err);
   }
 };
@@ -45,12 +18,13 @@ export const getSGG = async (req, res) => {
     params: { id },
   } = req;
 
-  const query = `SELECT CODE, SGGNAME FROM LOCSIGUNGU WHERE DOCODE = ${id}`;
+  const result = await db.Sgg.findAll({
+    where: { code_doe: Number(id) },
+    logging: false,
+  });
 
-  const [isError, result] = await getQueryResult(query);
-
-  if (!isError) {
-    res.status(statusCode.ok).json({ DO: id, SGG: result });
+  if (result) {
+    res.status(statusCode.ok).json({ DO: Number(id), SGG: result });
   } else {
     console.log(result);
     res.status(statusCode.err).send(serverMSG.server_err);
@@ -63,12 +37,13 @@ export const getEMD = async (req, res) => {
     params: { id },
   } = req;
 
-  const query = `SELECT CODE, EMDNAME FROM LOCINFO WHERE SGGCODE = ${id}`;
+  const result = await db.Emd.findAll({
+    where: { code_sgg: Number(id) },
+    logging: false,
+  });
 
-  const [isError, result] = await getQueryResult(query);
-
-  if (!isError) {
-    res.status(statusCode.ok).json({ SGG: id, EMD: result });
+  if (result) {
+    res.status(statusCode.ok).json({ SGG: Number(id), EMD: result });
   } else {
     console.log(result);
     res.status(statusCode.err).send(serverMSG.server_err);
