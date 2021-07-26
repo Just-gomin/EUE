ecaf9de882b758fecc81043aedbb2904d311f771
Spark <loot04@korea.ac.kr>
2021-07-26 15:54:31 +0900

    aircon 유지

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -4,9 +4,7 @@ import { Spinner, Row, Button } from 'react-bootstrap';
 function Loading() {
     return (
             <Button id='formbtn' className='d-flex justify-content-center align-items-center m-auto' style={{ width: '200px', height: '200px', flexDirection: 'column' }} disabled>
-                <Spinner animation="border" role="status">
-                    <span className="sr-only">Loading...</span>
-                </Spinner>
+                <Spinner animation="border" role="status"/>
                 <br />
                 Loading...
             </Button>
