f910891e8f41837c56388fdf559d2b75006a6f01
Spark <loot04@korea.ac.kr>
2021-07-28 11:09:58 +0900

    test 2

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -82,7 +82,6 @@ function UserInfo() {
                                     변경
                                 </Link>
                             </Button>
-                            
                         }
                     </Row>
                     {nickname &&
